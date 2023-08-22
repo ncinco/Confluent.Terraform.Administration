d21e64cfda972e3857b86f4f3b5435c08a4cc288
ncinco <ncinco@gmail.com>
2023-08-22 16:48:05 +1200

    vars

--- environments/variables.tf
+++ environments/variables.tf
@@ -3,16 +3,19 @@ variable "environment" {
   type        = string
 }
 
+# Azure
 variable "cloud" {
   description = "Name of environment"
   type        = string
 }
 
+# australiaeast
 variable "region" {
   description = "Name of environment"
   type        = string
 }
 
+# ESSENTIALS, ADVANCED
 variable "package" {
   description = "Name of environment"
   type        = string
