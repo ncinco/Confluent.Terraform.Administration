6d31a7e025c885c3af815756032e6790aae77557
ncinco <ncinco@gmail.com>
2023-08-23 10:54:01 +1200

    schema

--- environments/environments.tf
+++ environments/environments.tf
@@ -4,25 +4,3 @@ resource "confluent_environment" "environment" {
     prevent_destroy = true
   }
 }
-
-data "confluent_schema_registry_region" "schema_registry_region" {
-  cloud   = var.schema_registry.cloud
-  region  = var.schema_registry.region
-  package = var.schema_registry.package
-}
-
-resource "confluent_schema_registry_cluster" "schema_registry_cluster" {
-  package = data.confluent_schema_registry_region.schema_registry_region.package
-
-  environment {
-    id = confluent_environment.environment.id
-  }
-
-  region {
-    id = data.confluent_schema_registry_region.schema_registry_region.id
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
