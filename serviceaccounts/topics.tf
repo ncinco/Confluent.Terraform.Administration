2e035511c8a6797baf685a0ff4072873697ff566
ncinco <ncinco@gmail.com>
2023-08-23 17:04:13 +1200

    t

--- serviceaccounts/topics.tf
+++ serviceaccounts/topics.tf
@@ -6,7 +6,7 @@ resource "confluent_kafka_topic" "kafka_topics" {
   }
   topic_name         = each.value.name
   partitions_count   = each.value.partitions_count
-  rest_endpoint      = var.rest_endpoint
+  rest_endpoint      = "https://pkc-4n66v.australiaeast.azure.confluent.cloud:443"
   credentials {
     key              = "YEG5JUJY7HSTPQWV"
     secret           = "eKabVndAyySP/XkhZ4DPJ7qiVv+fsrCAGnL/9KLTj/i69Hm0U7RjouWnHe+53HiC"
