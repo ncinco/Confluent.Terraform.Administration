35643c2f1b8cfc1ae8e9a23cb4074b04c9201843
ncinco <ncinco@gmail.com>
2023-08-23 16:32:28 +1200

    t

--- serviceaccounts/topics.tf
+++ serviceaccounts/topics.tf
@@ -5,8 +5,8 @@ resource "confluent_kafka_topic" "orders" {
   topic_name         = "orders"
   rest_endpoint      = "https://pkc-4n66v.australiaeast.azure.confluent.cloud:443"
   credentials {
-    key    = "AX2D7PJ4PYLUBRL6"
-    secret = "zrt0g5L/KTE9YE9xhvhfhccBK9v117GCFtcXKLGVBIxwNlPT59/YOwlPvUgOX8rX"
+    key    = "FYV62S7ZL3DF3PTI"
+    secret = "DT6i/7HImfOrWDtXrT2M65Xg/5+6YO4hVf80zGgYprhDMm9FuaL2dxJpFlZO7YV3"
   }
 
   lifecycle {
