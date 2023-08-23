8a648e9f048499de6e7c4c8f5e0cee4ada56821c
ncinco <ncinco@gmail.com>
2023-08-23 15:53:12 +1200

    b

--- serviceaccounts/rolebindings.tf
+++ serviceaccounts/rolebindings.tf
@@ -1,13 +1,7 @@
-/* resource "confluent_role_binding" "role_binding_cluster" {
+resource "confluent_role_binding" "role_binding_cluster" {
   for_each    = { for role_bindings_cluster in var.role_bindings_clusters : role_bindings_cluster.request_id => role_bindings_cluster }
 
   principal   = "User:${each.value.principal}"
   role_name   = "${each.value.role_name}"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=${each.value.cluster_id}"
-} */
-
-/* resource "confluent_role_binding" "role_binding_cluster" {
-  principal   = "User:sa-68x1dq"
-  role_name   = "Operator"
-  crn_pattern = "crn://confluent.cloud/organization=4bcb09a9-ed92-465c-b6b6-3175ba61fe66/environment=env-gnz59v/cloud-cluster=lkc-r2ozw7"
-} */
+}
--- serviceaccounts/terraform.tfvars
+++ serviceaccounts/terraform.tfvars
@@ -1,17 +1,14 @@
-role_bindings_clusters = []
-
-/* role_bindings_clusters = [
+role_bindings_clusters = [
   {
     request_id  = "1"
-    principal   = "sa-68x1dq"
+    principal   = "sa-oznjmp"
     role_name   = "Operator"
-    cluster_id  = "lkc-r2ozw7"
-  }
-  ,
+    cluster_id  = "lkc-ny30qd"
+  },
   {
     request_id  = "2"
-    principal   = "sa-68x1dq"
+    principal   = "sa-oznjmp"
     role_name   = "Operator"
-    cluster_id  = "lkc-23q862"
+    cluster_id  = "lkc-3ng01j"
   }
-] */
+]
