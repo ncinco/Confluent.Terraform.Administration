f0e3fd68b24f02b853f4e373e41abf02ed6a72d1
ncinco <ncinco@gmail.com>
2023-08-23 12:56:38 +1200

    sa

--- serviceaccounts/servicesaccounts_prod.tf
+++ serviceaccounts/servicesaccounts_prod.tf
@@ -3,13 +3,13 @@ resource "confluent_service_account" "service_account_cluster_operator_prod" {
   description  = "Cluster Operator (prod)"
 }
 
-resource "confluent_role_binding" "role_binding_cluster_operator" {
+resource "confluent_role_binding" "role_binding_cluster_operator_prod_cluster_0" {
   principal   = "User:${confluent_service_account.service_account_cluster_operator_prod.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-ygj9w6"
 }
 
-resource "confluent_role_binding" "role_binding_cluster_operator" {
+resource "confluent_role_binding" "role_binding_cluster_operator_prod_cluster_1" {
   principal   = "User:${confluent_service_account.service_account_cluster_operator_prod.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-wzxywj"
