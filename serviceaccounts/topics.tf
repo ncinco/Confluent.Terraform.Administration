3d1004e264d28a822c500d9aea88dff3ae8b498c
ncinco <ncinco@gmail.com>
2023-08-23 14:20:27 +1200

    t

--- serviceaccounts/topics.tf
+++ serviceaccounts/topics.tf
@@ -1,3 +1,19 @@
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = "lkc-r2ozw7"
+  }
+  topic_name         = "orders"
+  rest_endpoint      = "https://pkc-4n66v.australiaeast.azure.confluent.cloud:443"
+  credentials {
+    key    = "IMXAE45QTPK7UCQB"
+    secret = "I7kRcZDy/39FMgltA1gRaMALGs4xlm0VSVbsrMKBu21HiMgab34AE44rp2uVbtTv"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "confluent_kafka_topic" "batman" {
   kafka_cluster {
     id = "lkc-r2ozw7"
