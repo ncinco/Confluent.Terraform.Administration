cdc136f45b2a233817dbe3cfe39f2335864205bd
ncinco <ncinco@gmail.com>
2023-08-23 13:11:59 +1200

    sa

--- serviceaccounts/servicesaccounts_dev.tf
+++ serviceaccounts/servicesaccounts_dev.tf
@@ -3,7 +3,7 @@ resource "confluent_service_account" "service_account_cluster_operator_dev" {
   description  = "Cluster Operator DEV"
 }
 
-resource "confluent_role_binding" "role_binding_cluster_operator_dev_cluster_0" {
+/* resource "confluent_role_binding" "role_binding_cluster_operator_dev_cluster_0" {
   principal   = "User:${confluent_service_account.service_account_cluster_operator_dev.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-r2ozw7"
@@ -13,4 +13,4 @@ resource "confluent_role_binding" "role_binding_cluster_operator_dev_cluster_1" 
   principal   = "User:${confluent_service_account.service_account_cluster_operator_dev.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-23q862"
-}
+} */
--- serviceaccounts/servicesaccounts_uat.tf
+++ serviceaccounts/servicesaccounts_uat.tf
@@ -3,7 +3,7 @@ resource "confluent_service_account" "service_account_cluster_operator_uat" {
   description  = "Cluster Operator UAT"
 }
 
-resource "confluent_role_binding" "role_binding_cluster_operator_uat_cluster_0" {
+/* resource "confluent_role_binding" "role_binding_cluster_operator_uat_cluster_0" {
   principal   = "User:${confluent_service_account.service_account_cluster_operator_uat.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-0j7869"
@@ -13,4 +13,4 @@ resource "confluent_role_binding" "role_binding_cluster_operator_uat_cluster_1" 
   principal   = "User:${confluent_service_account.service_account_cluster_operator_uat.id}"
   role_name   = "Operator"
   crn_pattern = "crn://confluent.cloud/organization=${var.organisation_id}/environment=${var.environment_id}/cloud-cluster=lkc-8mjq6m"
-}
+} */
